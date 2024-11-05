cd44992fb74d591c558466ae8c9d5f95c1da28c6
Eray <erayds@outlook.com>
2024-11-06 02:05:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const colors = require('colors');
-const title = require('./modules/title.js'); 
-const fastBomber = require('./modules/sms.js');
+const title = require('./title.js'); 
+const fastBomber = require('./sms.js');
 const app = express();
 const port = 3000;
-app.get('/send-sms', (req, res) => {
+app.get('/api', (req, res) => {
   const telefon = req.query.telefon;
   const miktar = req.query.miktar; 
   if (!telefon || telefon.length !== 10) {
-    return res.status(400).json({ error: 'Telefon Numarasi 10 Haneli Olmalidir. Ex: 5401234521' });
+    return res.status(400).json({ error: 'Telefon Numarasi 10 Haneli Olmalidir. Ex: 5xxxxxxxxx' });
   }
   if (!miktar || isNaN(miktar)) {
     return res.status(400).json({ error: 'Lutfen Geçerli Bir Miktar Giriniz' });
   }
-  title('Hosgeldiniz');
+  title('Hosgeldiniz Github https://github.com/kdmldeveloper/sms-bomber-api');
   title('Numara: ' + telefon);
-  title(`Telefon: ${telefon} - Miktar: ${miktar}`);
+  title(`Gsm: ${telefon} - Gönderilecek Miktar: ${miktar}`);
 
-  console.log('SMS Gonderiliyor...'.blue);
+  console.log('SMS Gönderiliyor...'.blue);
   fastBomber(telefon, miktar);
   res.json({
     message: `SMS'ler başarıyla gönderiliyor: ${telefon} - Miktar: ${miktar}`,
