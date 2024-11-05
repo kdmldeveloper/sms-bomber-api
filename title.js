db340eec35706aeaaaae96a4b822eee1c265d042
Eray <erayds@outlook.com>
2024-11-06 02:01:26 +0300

    title.js

--- title.js
+++ title.js
@@ -1,8 +1,8 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + " / FastBomber v1.0 - github.com/fastuptime";
+        process.title = x + "https://github.com/kdmldeveloper/sms-bomber-api";
     } else {
-        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/fastuptime" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / https://github.com/kdmldeveloper/sms-bomber-api" + '\x1b\x5c');
     }
 }
 
